9d519fc2709ac8fbe27bc2c54d433c4cc8af6e56
Bingyan Sahoo <bingyan.sahoo@intimetec.com>
2020-02-18 12:25:20 +0530

    changes

---
+++ src/main/java/com/test18022020/App.java
@@ -0,0 +1,13 @@
+package com.test18022020;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+
+    public static void main( String[] args )
+    {
+    }
+}
---
+++ src/main/java/com/test18022020/tetscalc.java
@@ -0,0 +1,9 @@
+package com.test18022020;
+
+public class tetscalc {
+	
+	public String getData(String str) {
+		return "Hello :" +str;
+	}
+
+}
